c158ea17fd0b1be4bcd13af48d4f396d2280019b
Sm0key9 <89950112+Sm0key9@users.noreply.github.com>
2022-08-29 12:59:44 +1000

    heheha
    sam is stinky and the lights light lights like lights light lights

--- Assets/DayNightCycle/LightingManager.cs
+++ Assets/DayNightCycle/LightingManager.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class LightingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Video up to 3:48
+    [SerializeField]private Light DirectionalLight;
+    [SerializeField]private LightingPreset Preset;
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        
+        if (DirectionalLight != null)
+            return;
+
+        if(RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach(Light light in lights)
+            {
+                if(light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                } 
+            }
+        }
     }
 }
